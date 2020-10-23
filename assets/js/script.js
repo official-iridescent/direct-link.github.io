0024c87e43090371ed1b7ef9ef7f1cc6ceae9ba1
sn0whuouV <mihsan2332@gmail.com>
2020-10-23 10:22:30 +0700

    menambahkan side form order

--- assets/js/script.js
+++ assets/js/script.js
@@ -84,3 +84,82 @@ function mostrar(e) {
     }
 
 }
+
+$( document ).ready(function() {
+    $('.cta-open').on('click', function() {
+      $('.toggle-form, .formwrap, .toggle-bg').addClass('active');
+      $('.icon-close').addClass('open');
+  });
+   $('.icon-close').on('click', function() {
+      $('.toggle-form, .formwrap, .toggle-bg').removeClass('active');
+      $('.icon-close').removeClass('open');
+  });
+});
+
+const btnSubmit = document.getElementById('btnSubmit');
+
+function closeForm() {
+    const link = document.getElementById('link');
+    const nleng = document.getElementById('nleng');
+    const nope = document.getElementById('nope');
+    const almt = document.getElementById('almt');
+    const kel = document.getElementById('kel');
+    const kec = document.getElementById('kec');
+    const kota = document.getElementById('kota');
+    const prov = document.getElementById('prov');
+    const pos = document.getElementById('pos');
+
+    link.value = '';
+    nleng.value = '';
+    nope.value = '';
+    almt.value = '';
+    kel.value = '';
+    kec.value = '';
+    kota.value = '';
+    prov.value = '';
+    pos.value = '';
+}
+
+function forwardMessage() {
+    const link = document.getElementById('link').value;
+    const nleng = document.getElementById('nleng').value;
+    const nope = document.getElementById('nope').value;
+    const almt = document.getElementById('almt').value;
+    const kel = document.getElementById('kel').value;
+    const kec = document.getElementById('kec').value;
+    const kota = document.getElementById('kota').value;
+    const prov = document.getElementById('prov').value;
+    const pos = document.getElementById('pos').value;
+    const pengiriman = document.getElementById('pengiriman').value;
+    const metpem = document.getElementById('metpem').value;
+
+    const message = `Hallo kak, aku mau order barang ini dong❤️%0A%0ALink Barang : ${link}%0ANama Lengkap : ${nleng}%0ANomor HP : ${nope}%0AAlamat : ${almt}%0A- Kelurahan : ${kel}%0A- Kecamatan : ${kec}%0A- Kota/Kabupaten : ${kota}%0A- Provinsi : ${prov}%0A- Kode POS : ${pos}%0APengiriman : ${pengiriman}%0AMetode Pembayaran : ${metpem}%0A
+    `;
+
+    window.open(`https://api.whatsapp.com/send?phone=+6288219778851&text=${message}`);
+    this.closeForm();
+}
+
+
+setInterval(() => {
+    const baseMessage = {
+        link: document.getElementById('link').value,
+        nleng: document.getElementById('nleng').value,
+        nope: document.getElementById('nope').value,
+        almt: document.getElementById('almt').value,
+        kel: document.getElementById('kel').value,
+        kec: document.getElementById('kec').value,
+        kota: document.getElementById('kota').value,
+        prov: document.getElementById('prov').value,
+        pos: document.getElementById('pos').value
+    }
+
+    for ( const item of Object.values(baseMessage) ) {
+        if ( item == '' ) {
+            btnSubmit.setAttribute('disabled', true);
+            break;
+        } else {
+            btnSubmit.removeAttribute('disabled');
+        }
+    }
+})
